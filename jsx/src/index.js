192b23e1ff6bfb440226468341cd441268909139
Jamen <yhatotz19.ja@gmail.com>
2020-04-03 13:39:54 +0800

    JSX Module

---
+++ jsx/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const buttonText = { text: 'Click Me!' };
+  const labelName = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelName}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+)
