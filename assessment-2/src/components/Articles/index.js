d5fa511dd617c5e6ed0a9ecfe76fd0069e586c68
Jamen <yhatotz19.ja@gmail.com>
2020-04-24 13:09:56 +0800

    Added condition for the image

--- assessment-2/src/components/Articles/index.js
+++ assessment-2/src/components/Articles/index.js
@@ -1,32 +1,42 @@
 import React from 'react';
 
-const Articles = (props) => {
-	return (
-		<div className="ui items">
-	   	{props.articles.map((article, idx) => (
-			  <div className="item" key={idx} style={{ marginBottom: '30px' }}>
-			    <div className="image">
-			      <img src={article.urlToImage} alt="" />
-			    </div>
-			    <div className="content">
-			      <a
-			      	className="header"
-			      	rel="noopener noreferrer"
-			      	target="_blank"
-			      	href={article.url}
-			      >
-			      	{article.title}
-			      </a>
-			      <div className="description">
-			        <p>
-								{article.description}
-			        </p>
-			      </div>
-			    </div>
-			  </div>
-	    ))}
-		</div>
-	);
+class Articles extends React.Component {
+	renderImage(urlToImage) {
+		if (urlToImage) {
+			return (
+				<div className="image">
+		      <img src={urlToImage} alt="" />
+		    </div>
+			);
+		}
+	}
+
+	render() {
+		return (
+			<div className="ui items">
+		   	{this.props.articles.map((article, idx) => (
+				  <div className="item" key={idx} style={{ marginBottom: '30px' }}>
+				    {this.renderImage(article.urlToImage)}
+				    <div className="content">
+				      <a
+				      	className="header"
+				      	rel="noopener noreferrer"
+				      	target="_blank"
+				      	href={article.url}
+				      >
+				      	{article.title}
+				      </a>
+				      <div className="description">
+				        <p>
+									{article.description}
+				        </p>
+				      </div>
+				    </div>
+				  </div>
+		    ))}
+			</div>
+		);
+	}
 }
 
 export default Articles;
